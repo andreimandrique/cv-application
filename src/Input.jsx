16a0026282f509f4628546f3f23a134f0154fc6f
andrei <andreimandrique3@gmail.com>
2024-10-17 20:17:04 +0800

    add logic

--- src/Input.jsx
+++ src/Input.jsx
@@ -11,25 +11,34 @@ function Display({ isRegistered, children }) {
 function Input() {
   const [value, setValue] = useState("");
   const [show, setShow] = useState(false);
+  const [showInput, setShowInput] = useState(true);
 
   function handleName() {
     console.log(value);
 
     setShow(true);
+    setShowInput(false);
+  }
+
+  function handleEdit() {
+    setShow(false);
+    setShowInput(true);
   }
 
   return (
     <>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      ></input>
-
-      <button onClick={handleName}>Submit</button>
+      <Display isRegistered={showInput}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        ></input>
+        <button onClick={handleName}>Submit</button>
+      </Display>
 
       <Display isRegistered={show}>
         <h1>{value}</h1>
+        <button onClick={handleEdit}>Edit</button>
       </Display>
     </>
   );
